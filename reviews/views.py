fd2106b65ef5fbe1b94b66e4b0483e3a360f6aa5
s3nt1n3lz21 <s3nt1n3lz18@gmail.com>
2023-09-14 17:12:56 +0100

    Now using Sessions to store the users favorite review

--- reviews/views.py
+++ reviews/views.py
@@ -79,6 +79,14 @@ class ReviewDetailView(DetailView):
     template_name = "reviews/review_detail.html"
     model = Review
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        loaded_review = self.object
+        request = self.request
+        favorite_id = request.session.get("favorite_review")
+        context["is_favorite"] = favorite_id == str(loaded_review.id)
+        return context
+
     # def get_context_data(self, **kwargs):
     #     context = super().get_context_data(**kwargs)
     #     review_id = kwargs["id"]
@@ -86,4 +94,8 @@ class ReviewDetailView(DetailView):
     #     context["review"] = review
     #     return context
     
-    
+class AddFavoriteView(View):
+    def post(self, request):
+        review_id = request.POST["review_id"]
+        request.session["favorite_review"] = review_id
+        return HttpResponseRedirect("/reviews/" + review_id)
