fb87309fd2a1e8cd091813cf1b1bd7433cb547dd
s3nt1n3lz21 <s3nt1n3lz18@gmail.com>
2023-09-14 11:43:34 +0100

    Added a template class view

--- reviews/views.py
+++ reviews/views.py
@@ -1,6 +1,8 @@
+from typing import Any
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.views import View
+from django.views.generic.base import TemplateView
 
 from .forms import ReviewForm
 
@@ -40,5 +42,13 @@ class ReviewView(View):
 #         "form": form
 #     })
 
-def thank_you(request):
-    return render(request, "reviews/thank_you.html")
+class ThankYouView(TemplateView):
+    template_name = "reviews/thank_you.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["message"] = "This works!"
+        return context
+
+# def thank_you(request):
+#     return render(request, "reviews/thank_you.html")
