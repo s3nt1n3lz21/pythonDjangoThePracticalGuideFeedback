aaa7bf564e80ee26e47dd7ff4d940dd5fc285bd0
s3nt1n3lz21 <s3nt1n3lz18@gmail.com>
2023-09-14 11:52:20 +0100

    Added a review list page using a  class view TemplateView

--- reviews/views.py
+++ reviews/views.py
@@ -5,6 +5,7 @@ from django.views import View
 from django.views.generic.base import TemplateView
 
 from .forms import ReviewForm
+from .models import Review
 
 # Create your views here.
 
@@ -52,3 +53,13 @@ class ThankYouView(TemplateView):
 
 # def thank_you(request):
 #     return render(request, "reviews/thank_you.html")
+
+class ReviewsListView(TemplateView):
+    template_name = "reviews/review_list.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        reviews = Review.objects.all()
+        context["reviews"] = reviews
+        return context
+    
